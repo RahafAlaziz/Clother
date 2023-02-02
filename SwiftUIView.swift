4c2b15a0c310019bdccde5d4644935af6813dc58
rana <alzahraniranah@gmail.com>
2023-02-02 04:35:09 +0300

    second commit

---
+++ SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  project3
+//
+//  Created by Rana on 11/07/1444 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Rana")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
