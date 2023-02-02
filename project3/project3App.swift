c32a655dc544ee578ccbed6e44e7e2fcd2c372c4
rana <alzahraniranah@gmail.com>
2023-02-02 04:13:24 +0300

    first commit

---
+++ project3/project3App.swift
@@ -0,0 +1,17 @@
+//
+//  project3App.swift
+//  project3
+//
+//  Created by Rana on 11/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
